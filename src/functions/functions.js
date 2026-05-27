e4dac0f9f26c86f7a2fa5a256368aedb19646496
Morne <37689597+morneblake@users.noreply.github.com>
2026-05-27 08:33:12 +0200

    Removed additional functions
    Removed additional functions from src/functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,70 +1,7 @@
 ﻿/* eslint-disable @typescript-eslint/no-unused-vars */
 /* global console setInterval, clearInterval */
 
-/**
- * Add two numbers
- * @customfunction
- * @param {number} first First number
- * @param {number} second Second number
- * @returns {number} The sum of the two numbers.
- */
-function add(first, second) {
-  return first + second;
-}
 
-/**
- * Displays the current time once a second
- * @customfunction
- * @param {CustomFunctions.StreamingInvocation<string>} invocation Custom function invocation
- */
-function clock(invocation) {
-  const timer = setInterval(() => {
-    const time = currentTime();
-    invocation.setResult(time);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Returns the current time
- * @returns {string} String with the current time formatted for the current locale.
- */
-function currentTime() {
-  return new Date().toLocaleTimeString();
-}
-
-/**
- * Increments a value once a second.
- * @customfunction
- * @param {number} incrementBy Amount to increment
- * @param {CustomFunctions.StreamingInvocation<number>} invocation
- */
-function increment(incrementBy, invocation) {
-  let result = 0;
-  const timer = setInterval(() => {
-    result += incrementBy;
-    invocation.setResult(result);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Writes a message to console.log().
- * @customfunction LOG
- * @param {string} message String to write.
- * @returns String to write.
- */
-function logMessage(message) {
-  console.log(message);
-
-  return message;
-}
 
 const SQL_MI_SKUS = [
   {"name":"General Purpose - Standard Series (Gen5)", "typeId":1, "vCore":4, "ram":20, "ramMultiplier":5.1, "iopsMax":7500, "iopsDefault":500, "throughputMax":250, "storageMaxInMb":2048, "costPerHour":1.01},
